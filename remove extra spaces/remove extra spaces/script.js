2d69e8af3091d4c0f759bcec88fd253feab3110d
Mr.Navid <124199694+navidmsh@users.noreply.github.com>
2023-04-30 23:27:35 +0330

    Delete remove extra spaces/remove extra spaces directory

--- remove extra spaces/remove extra spaces/script.js
+++
@@ -1,5 +0,0 @@
-function removeSpaces() {
-            let originalText = document.getElementById("originalText").value;
-            let newText = originalText.replaceAll(" ", "");
-            document.getElementById("output").innerHTML = newText;
-}
